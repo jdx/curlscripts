ad401c565bd064300eaaca47090a042488a0c397
dickeyxxx <jeff@dickeyxxx.com>
2014-05-30 21:36:19 -0700

    hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var PORT = 3000;
+
+app.get('/', function(req, res) {
+  setTimeout(function() {
+    res.send(200, "curl scripts!");
+  }, 200);
+});
+
+app.listen(PORT, function(c) {
+  console.log("live on port", PORT);
+});
